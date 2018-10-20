9f34f813907a405137afcae74ca2957fc99821bc
Faisal <taifaisal57@gmail.com>
2018-10-20 09:51:26 +0530

    updated code

--- app/src/main/java/com/akeedtest/android/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/akeedtest/android/adapters/RestaurantListAdapter.java
@@ -1,15 +1,19 @@
 package com.akeedtest.android.adapters;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.akeedtest.android.R;
 import com.akeedtest.android.listeners.OnRestaurantSelectListener;
 import com.akeedtest.android.models.RestaurantListModel;
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -17,20 +21,28 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
 
     private List<RestaurantListModel> restaurantList;
     private OnRestaurantSelectListener onRestaurantSelectListener;
+    private Context context;
 
-    public RestaurantListAdapter(List<RestaurantListModel> restaurantList, OnRestaurantSelectListener onRestaurantSelectListener) {
+    public RestaurantListAdapter(List<RestaurantListModel> restaurantList, OnRestaurantSelectListener onRestaurantSelectListener,
+                                 Context context) {
         this.restaurantList = restaurantList;
         this.onRestaurantSelectListener = onRestaurantSelectListener;
+        this.context = context;
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView tv_car_name;
-        LinearLayout ln_container;
+        ImageView iv_res_banner;
+        TextView tv_res_name, tv_res_area, tv_res_cuisines, tv_duration;
+        //LinearLayout ln_container;
 
         MyViewHolder(View view) {
             super(view);
-            //tv_car_name = view.findViewById(R.id.tv_car_name);
+            iv_res_banner = view.findViewById(R.id.iv_res_banner);
+            tv_res_name = view.findViewById(R.id.tv_res_name);
+            tv_res_area = view.findViewById(R.id.tv_res_area);
+            tv_res_cuisines = view.findViewById(R.id.tv_res_cuisines);
+            tv_duration = view.findViewById(R.id.tv_duration);
         }
     }
 
@@ -45,15 +57,27 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         final RestaurantListModel restaurantListModel = restaurantList.get(position);
-        //holder.tv_car_name.setText(restaurantListModel.getCar_model());
 
-        holder.ln_container.setOnClickListener(new View.OnClickListener() {
+        Glide.with(context).load(restaurantListModel.getRes_image_url()).into(holder.iv_res_banner);
+        holder.tv_res_name.setText(restaurantListModel.getRes_name());
+        holder.tv_res_area.setText(restaurantListModel.getArea());
+        holder.tv_duration.setText(restaurantListModel.getDelivery_time());
+
+        String cuisine = restaurantListModel.getCuisines()[0];
+        holder.tv_res_cuisines.setText(cuisine);
+
+        int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+        int imageHeight = screenHeight/6;
+        holder.iv_res_banner.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, imageHeight));
+        //int width = displayMetrics.widthPixels;
+
+        /*holder.ln_container.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //CommonUtilities.getDefaultFragmentManager().popBackStack();
                 onRestaurantSelectListener.onRestaurantSelect(restaurantListModel);
             }
-        });
+        });*/
 
     }
 
