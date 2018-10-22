1617bec45a5890745e79bd3bf93a13b2a32606cc
Faisal <taifaisal57@gmail.com>
2018-10-22 19:22:35 +0530

    Added cart view

--- app/src/main/java/com/akeedtest/android/MenuListActivity.java
+++ app/src/main/java/com/akeedtest/android/MenuListActivity.java
@@ -1,11 +1,14 @@
 package com.akeedtest.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -15,6 +18,7 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.akeedtest.android.adapters.MenuListAdapter;
 import com.akeedtest.android.adapters.RestaurantListAdapter;
@@ -33,12 +37,22 @@ public class MenuListActivity extends AppCompatActivity implements OnMenuSelectL
     private RestaurantListModel restaurantListModel;
     private Activity activity;
     private ImageView iv_res_banner;
+    private RelativeLayout rl_header;
+    private TabLayout tabs;
+    private CardView card_cart;
 
     ArrayList<MenuListModel> menuList;
     RecyclerView rcv_menu_list;
     ProgressBar progress_bar;
-    TextView tv_empty_data;
+    TextView tv_empty_data, tv_title, tv_delivery_time, tv_cart_price, tv_view_cart;
     MenuListAdapter menuListAdapter;
+    MenuListResponseModel menuListResponseModel;
+    int cartPrice = 0;
+
+    String ITEM_SOUP = "Soup";
+    String ITEM_STARTER = "Starter";
+    String ITEM_MAIN_COURSE = "Main Course";
+    String ITEM_DESSERT = "Dessert";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +68,31 @@ public class MenuListActivity extends AppCompatActivity implements OnMenuSelectL
 
         initComponent();
 
-        getMenuList();
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tabs.getSelectedTabPosition() == 0)
+                    getMenuList(ITEM_SOUP);
+                else if (tabs.getSelectedTabPosition() == 1)
+                    getMenuList(ITEM_STARTER);
+                else if (tabs.getSelectedTabPosition() == 2)
+                    getMenuList(ITEM_MAIN_COURSE);
+                else
+                    getMenuList(ITEM_DESSERT);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        getMenuList(ITEM_SOUP);
 
         //Toast.makeText(activity, "Res ID : " + res_id, Toast.LENGTH_LONG).show();
     }
@@ -65,9 +103,14 @@ public class MenuListActivity extends AppCompatActivity implements OnMenuSelectL
         rcv_menu_list = findViewById(R.id.rcv_menu_list);
         progress_bar = findViewById(R.id.progress_bar);
         tv_empty_data = findViewById(R.id.tv_empty_data);
+        tv_title = findViewById(R.id.tv_title);
         iv_res_banner = findViewById(R.id.iv_res_banner);
-
-        View view = findViewById(R.id.view_shade);
+        rl_header = findViewById(R.id.rl_header);
+        tv_delivery_time = findViewById(R.id.tv_delivery_time);
+        tabs = findViewById(R.id.tabs);
+        card_cart = findViewById(R.id.card_cart);
+        tv_cart_price = findViewById(R.id.tv_cart_price);
+        tv_view_cart = findViewById(R.id.tv_view_cart);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(activity);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
@@ -77,95 +120,74 @@ public class MenuListActivity extends AppCompatActivity implements OnMenuSelectL
 
         int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
         int imageHeight = screenHeight/4;
-        iv_res_banner.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, imageHeight));
-        view.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, imageHeight));
+        //iv_res_banner.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, imageHeight));
+        rl_header.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, imageHeight));
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         // add back arrow to toolbar
         if (getSupportActionBar() != null){
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            //getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            //getSupportActionBar().setDisplayShowHomeEnabled(false);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
+        toolbar.setTitle("");
+
+        toolbar.setNavigationIcon(R.drawable.ic_back_arrow_white);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        tv_title.setText(restaurantListModel.getRes_name());
+        tv_delivery_time.setText(restaurantListModel.getDelivery_time());
+
+        addTab(ITEM_SOUP);
+        addTab(ITEM_STARTER);
+        addTab(ITEM_MAIN_COURSE);
+        addTab(ITEM_DESSERT);
+
+        tv_view_cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(activity, "Cart view clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
-    private void getMenuList() {
+    private void addTab(String title) {
+        tabs.addTab(tabs.newTab().setText(title));
+        //mPagerAdapter.addTabPage(title);
+    }
+
+    private void getMenuList(String itemType) {
         try {
             progress_bar.setVisibility(View.VISIBLE);
             rcv_menu_list.setVisibility(View.GONE);
 
             GetMenuListService getMenuListService = new GetMenuListService(activity, res_id);
-            createGetMenuListRequest(getMenuListService);
-            getMenuListService.makeRequest();
+            createGetMenuListRequest(getMenuListService, itemType);
+            if (menuList.size() < 1)
+                getMenuListService.makeRequest();
+            else
+                setMenuData(itemType, menuListResponseModel);
         }
         catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    private void createGetMenuListRequest(GetMenuListService getMenuListService) {
+    private void createGetMenuListRequest(GetMenuListService getMenuListService, final String itemType) {
         getMenuListService.setOnResponseListener(new GetMenuListService.OnResponseListener() {
             @Override
             public void onResponse(MenuListResponseModel menuListResponseModel) {
                 try {
-                    if (progress_bar != null && progress_bar.getVisibility() == View.VISIBLE)
-                        progress_bar.setVisibility(View.GONE);
-
-                    if (menuListResponseModel.isSuccess()){
-                        menuList = menuListResponseModel.getMenuList();
-
-                        if (menuList.size() == 0){
-                            tv_empty_data.setVisibility(View.VISIBLE);
-                            rcv_menu_list.setVisibility(View.GONE);
-                            tv_empty_data.setText(R.string.empty_data);
-                        }
-                        else {
-
-                            // Adding mock data as per the Restaurant ID, in real project curated menu list should come from
-                            // server as per the Restaurant ID
-                            ArrayList<MenuListModel> resMenu = new ArrayList<>();
-                            for (MenuListModel menu : menuList){
-                                if (menu.getRes_id() == res_id){
-                                    resMenu.add(menu);
-                                }
-                            }
-
-                            tv_empty_data.setVisibility(View.GONE);
-                            rcv_menu_list.setVisibility(View.VISIBLE);
-
-                            menuListAdapter = new MenuListAdapter(resMenu, MenuListActivity.this, activity);
-                            menuListAdapter.notifyDataSetChanged();
-                            rcv_menu_list.setAdapter(menuListAdapter);
-                        }
-                    }
-                    else {
-                        tv_empty_data.setVisibility(View.VISIBLE);
-                        rcv_menu_list.setVisibility(View.GONE);
-                        tv_empty_data.setText(R.string.empty_data);
-
-                        AlertDialog.Builder builder;
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                            builder = new AlertDialog.Builder(activity, android.R.style.Theme_Material_Dialog_Alert);
-                        } else {
-                            builder = new AlertDialog.Builder(activity);
-                        }
-
-                        /*builder.setTitle("Cars")
-                                .setMessage(menuListResponseModel.getResponseMessage())
-                                .setPositiveButton("Try Again", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        getMenuList();
-                                    }
-                                })
-                                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        // do nothing
-                                    }
-                                })
-                                .setIcon(android.R.drawable.ic_dialog_alert)
-                                .show();*/
-                    }
+                    setMenuData(itemType, menuListResponseModel);
                 }
                 catch (Exception e){
                     e.printStackTrace();
@@ -176,8 +198,52 @@ public class MenuListActivity extends AppCompatActivity implements OnMenuSelectL
         });
     }
 
+    private void setMenuData(String itemType, MenuListResponseModel menuListResponseModel) {
+        this.menuListResponseModel = menuListResponseModel;
+        if (progress_bar != null && progress_bar.getVisibility() == View.VISIBLE)
+            progress_bar.setVisibility(View.GONE);
+
+        if (menuListResponseModel.isSuccess()){
+            menuList = menuListResponseModel.getMenuList();
+
+            if (menuList.size() == 0){
+                tv_empty_data.setVisibility(View.VISIBLE);
+                rcv_menu_list.setVisibility(View.GONE);
+                tv_empty_data.setText(R.string.empty_data);
+            }
+            else {
+
+                // Adding mock data as per the Restaurant ID, in real project curated menu list should come from
+                // server as per the Restaurant ID
+                ArrayList<MenuListModel> resMenu = new ArrayList<>();
+                for (MenuListModel menu : menuList){
+                    if (menu.getRes_id() == res_id && menu.getItem_type().equals(itemType)){
+                        resMenu.add(menu);
+                    }
+                }
+
+                tv_empty_data.setVisibility(View.GONE);
+                rcv_menu_list.setVisibility(View.VISIBLE);
+
+                menuListAdapter = new MenuListAdapter(resMenu, MenuListActivity.this, activity);
+                menuListAdapter.notifyDataSetChanged();
+                rcv_menu_list.setAdapter(menuListAdapter);
+            }
+        }
+        else {
+            tv_empty_data.setVisibility(View.VISIBLE);
+            rcv_menu_list.setVisibility(View.GONE);
+            tv_empty_data.setText(R.string.empty_data);
+        }
+    }
+
     @Override
     public void onMenuSelect(MenuListModel menuListModel) {
+        //Toast.makeText(activity, menuListModel.getItem_name(),Toast.LENGTH_LONG).show();
+        cartPrice += menuListModel.getItem_price() + 18;
+        card_cart.setVisibility(View.VISIBLE);
+
+        tv_cart_price.setText("Rs. " + cartPrice);
 
     }
 
